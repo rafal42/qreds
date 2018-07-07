7699a5f6d77784400a85103d8822a119d30eed2d
Rafał Warzocha <rafal.warzocha@netguru.co>
2018-07-07 15:30:19 +0200

    Move docs

--- lib/qreds/endpoint.rb
+++ lib/qreds/endpoint.rb
@@ -1,6 +1,6 @@
 module Qreds
+  # A container for helper methods to invoke reducers.
+  # The methods are defined via configuration (Config.define_reducer)
   module Endpoint
-    # A container for helper methods to invoke reducers.
-    # The methods are defined via configuration (Config.define_reducer)
   end
 end
